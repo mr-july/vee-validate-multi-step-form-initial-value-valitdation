9be167057c1b5c1350ea37158eb6ec2cc79b6e58
mr. JULY <mr_july@mail.ru>
2023-10-29 10:38:36 +0000

    feat: display errors for `notes` field twice (with `ErrorMessage` component and custom from `errors` property of the `useField`)

--- src/components/NotesSection.vue
+++ src/components/NotesSection.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ErrorMessage, useField } from 'vee-validate'
-import StringArray from './StringArray.vue'
+import { ErrorMessage, useField } from "vee-validate";
+import StringArray from "./StringArray.vue";
 
-const props = defineProps<{ isActive: boolean }>()
-const { value: notesValue } = useField<Array<string>>('notes')
+const props = defineProps<{ isActive: boolean }>();
+const { value: notesValue, errors } = useField<Array<string>>("notes");
 </script>
 
 <template>
@@ -11,5 +11,11 @@ const { value: notesValue } = useField<Array<string>>('notes')
     <label for="notes">Notes</label>
     <string-array v-model="notesValue" />
     <ErrorMessage name="notes" />
+    <div v-if="errors.length">
+      Errors:
+      <ul>
+        <li v-for="error in errors" :key="error" v-text="error" />
+      </ul>
+    </div>
   </fieldset>
 </template>
